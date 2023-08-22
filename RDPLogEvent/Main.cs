807944c6617bb156a9014518e8c691da19d8eb42
최원식 <cadman@naver.com>
2023-08-22 21:35:16 +0900

    home

--- RDPLogEvent/Main.cs
+++ RDPLogEvent/Main.cs
@@ -7,6 +7,7 @@ using System.Diagnostics;
 using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -86,9 +87,11 @@ namespace RDPLogEvent
             listView1.Columns.Add("WorkstationName", 150);
             listView1.Columns.Add("UserName", 120);
             listView1.Columns.Add("TimeCreated", 150);
+            listView1.Columns.Add("Type", 50);            
+
 
             //statusStrip1.RightToLeft = RightToLeft.Yes;
-            
+
             toolStripStatusLabel1.Text = "time:";
             toolStripStatusLabel2.Text = "OK";
 
@@ -96,48 +99,52 @@ namespace RDPLogEvent
             List<KeyValuePair<string, string>> data = new List<KeyValuePair<string, string>>();
 
             DDLCertify.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            data.Clear();
             data.Add(new KeyValuePair<string, string>("사용자 인증 실패", "4625"));
-            data.Add(new KeyValuePair<string, string>("사용자 인증 성공", "ASC"));
+            data.Add(new KeyValuePair<string, string>("사용자 인증 성공", "4624"));
             DDLCertify.DataSource = null;            
             DDLCertify.DataSource = new BindingSource(data, null);
             DDLCertify.DisplayMember = "Key";
             DDLCertify.ValueMember = "Value";
             DDLCertify.SelectedValue = "4625";
 
-
+            List<KeyValuePair<string, bool>> data2 = new List<KeyValuePair<string, bool>>();
             DDLOrderBy.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            data.Clear();
-            data.Add(new KeyValuePair<string, string>("최근순", "DESC"));
-            data.Add(new KeyValuePair<string, string>("오래된순", "ASC"));
+            data2.Clear();
+            data2.Add(new KeyValuePair<string, bool>("최근순", true));
+            data2.Add(new KeyValuePair<string, bool>("오래된순", false));
             DDLOrderBy.DataSource = null;
-            DDLOrderBy.DataSource = new BindingSource(data, null);
+            DDLOrderBy.DataSource = new BindingSource(data2, null);
             DDLOrderBy.DisplayMember = "Key";
             DDLOrderBy.ValueMember = "Value";
-            DDLOrderBy.SelectedValue = "DESC";
+            DDLOrderBy.SelectedValue = true;
 
+            List<KeyValuePair<string, int>> data3 = new List<KeyValuePair<string, int>>();
             DDLMaxRow.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            data.Clear();
-            data.Add(new KeyValuePair<string, string>("500건 조회", "500"));
-            data.Add(new KeyValuePair<string, string>("1000건 조회", "1000"));
-            data.Add(new KeyValuePair<string, string>("5000건 조회", "5000"));
-            data.Add(new KeyValuePair<string, string>("10000건 조회", "10000"));
+            data3.Clear();            
+            data3.Add(new KeyValuePair<string, int>("500건 조회", 500));
+            data3.Add(new KeyValuePair<string, int>("1000건 조회", 1000));
+            data3.Add(new KeyValuePair<string, int>("5000건 조회", 5000));
+            data3.Add(new KeyValuePair<string, int>("10000건 조회", 10000));
+            data3.Add(new KeyValuePair<string, int>("전체조회", 0));
             DDLMaxRow.DataSource = null;
-            DDLMaxRow.DataSource = new BindingSource(data, null);
+            DDLMaxRow.DataSource = new BindingSource(data3, null);
             DDLMaxRow.DisplayMember = "Key";
             DDLMaxRow.ValueMember = "Value";
-            DDLMaxRow.SelectedValue = "1000";
+            DDLMaxRow.SelectedValue = 1000;
 
         }
 
 
-        private int limitCount = 1000000;
 
         private void btnLog_Click(object sender, EventArgs e)
         {
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
-
+            #region 이전소스
+            /**
             int currentCount = 0;
+            int limitCount = 100000;
             string QueryString = @"*[System/EventID=4625]";
             var eventQuery = new EventLogQuery("Security", PathType.LogName, QueryString);
             eventQuery.ReverseDirection = true;    // 이벤트 최신 or 오래된순서 (order by)
@@ -181,6 +188,17 @@ namespace RDPLogEvent
             }
 
             listView1.Items.AddRange(items.ToArray());
+            **/
+            #endregion
+
+            listView1.Items.Clear();
+
+            string EventID = (string)DDLCertify.SelectedValue;
+            bool ReverseDirection = (bool)DDLOrderBy.SelectedValue;
+            int MaxCount = (int)DDLMaxRow.SelectedValue;
+
+            List<ListViewItem> items = EventDataQuery(EventID, ReverseDirection, MaxCount);
+            listView1.Items.AddRange(items.ToArray());
 
             stopwatch.Stop();
             toolStripStatusLabel1.Text = string.Format("time : {0} ms", stopwatch.ElapsedMilliseconds);
@@ -188,6 +206,64 @@ namespace RDPLogEvent
 
         } //btnLog_Click (e)
 
+
+
+        /// <summary>
+        /// 이벤트 로그 조회
+        /// </summary>
+        /// <param name="EventID"></param>
+        /// <param name="ReverseDirection"></param>
+        /// <param name="MaxCount"></param>
+        /// <returns></returns>
+        private List<ListViewItem> EventDataQuery(string EventID, bool ReverseDirection, int MaxCount)
+        {
+            int currentCount = 0;
+            string QueryString = string.Format(@"*[System/EventID={0}]", EventID);
+            var eventQuery = new EventLogQuery("Security", PathType.LogName, QueryString);
+            eventQuery.ReverseDirection = ReverseDirection;    // 이벤트 최신=true or 오래된순서=false (order by)
+
+            EventLogReader logReader = new EventLogReader(eventQuery);
+
+            var items = new List<ListViewItem>();
+            ListViewItem item;
+
+            //label1.Text = ((EventRecord)logEntry).c
+            ///((EventLogReader)logReader).
+
+            for (EventRecord logEntry = logReader.ReadEvent(); logEntry != null; logEntry = logReader.ReadEvent())
+            {
+                // Read Event details
+                var loginEventPropertySelector = new EventLogPropertySelector(new[] {
+                                "Event/EventData/Data[@Name='IpAddress']",
+                                "Event/EventData/Data[@Name='WorkstationName']",
+                                "Event/EventData/Data[@Name='TargetUserName']",
+                                "Event/System/TimeCreated/@SystemTime",
+                                "Event/EventData/Data[@Name='LogonType']"
+                            });
+                IList<object> logEventProperties = ((System.Diagnostics.Eventing.Reader.EventLogRecord)logEntry).GetPropertyValues(loginEventPropertySelector);
+
+
+                item = new ListViewItem(new[] {
+                    currentCount.ToString(),
+                    (string)logEventProperties[0],
+                    (string)logEventProperties[1],
+                    (string)logEventProperties[2],
+                    logEventProperties[3].ToString(),
+                    logEventProperties[4].ToString()
+                });
+
+                items.Add(item);
+
+                //listView1.Items.Add(item);
+
+                if ((++currentCount >= MaxCount) && (MaxCount > 0))
+                {
+                    break;
+                }
+            }
+
+            return items;
+        }
 
 
     } // public class (e)
